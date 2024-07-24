63b68b56770fd0e8895032a080cce4a4b4ae07a7
Vania Abbas <vaniaabbas2@gmail.com>
2024-07-24 15:16:17 +0500

    added a toolbar on the new note page

---
+++ script.js
@@ -0,0 +1,19 @@
+function formatText(command) {
+    document.execCommand(command, false, null);
+  }
+  
+  function openCalculator() {
+    alert('Open Calculator');
+    // Implement calculator logic here
+  }
+  
+  function openEquationSolver() {
+    alert('Open Equation Solver');
+    // Implement equation solver logic here
+  }
+  
+  function openGraphicalCalculator() {
+    alert('Open Graphical Calculator');
+    // Implement graphical calculator logic here
+  }
+  
